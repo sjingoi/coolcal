2022b00ec81ed9bf799ab8dc400205569711842f
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-06-05 23:08:41 -0400

    Added stuff to calendar

--- client/src/components/calendar/Calendar.tsx
+++ client/src/components/calendar/Calendar.tsx
@@ -4,15 +4,20 @@ import './calendar.css'
 
 function Calendar() {
     const days: String[] = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"]
+
     return (
         <div className="calendar">
             <div className="days-header">
                 {days.map(day=>(  <HeaderDay name={day}/>  ))}
             </div>
             {/* <div><div className="header-spacer"></div></div> */}
+            <div className="row-lines">
+                <div className="row-line"></div>
+            </div>
             <div className="timetable">
                 {days.map(day=>(  <Day name={day}/>  ))}
             </div>
+            
         </div>
     )
 }
